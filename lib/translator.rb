5fee4ccbf786df86fba7f672f02a847c65fe6ea8
Iain Robertson <iainrobertzon@gmail.com>
2020-08-15 20:19:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -26,6 +26,7 @@ def get_japanese_emoticon(file_path,emoticon)
      # return "Sorry, that emoticon was not found"
     #end 
   end
+  return "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning(file_path, emoticon)
